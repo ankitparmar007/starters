5c433a69899df7a675c340670b8b9c1f4f83c03a
ankitparmar007 <73648141+ankitparmar007@users.noreply.github.com>
2022-05-07 21:02:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from telethon import TelegramClient, events
-
+from get_course_time import getTime
 api_id = 1220132
 api_hash = 'bce9981f5e11687d0674531e1ca6f9b1'
 
@@ -12,9 +12,11 @@ print("program running")
 @teleclient.on(events.NewMessage(chats=source_list))
 async def my_event_handler(event):
     print("teleclient events called")
-    msg = event.message.message
+#   msg = event.message.message
+    msg = getTime(event.message.message)
     print(f"{msg = }")
-    await teleclient.send_message("givemedata2",msg)
+    
+    await teleclient.send_message("givemedata2",str(msg))
 
 def run():
     print("run called")
